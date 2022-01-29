1ce3db7bf0598735b6ee609c3656893c544939c3
hinzwifi <markjoshuahaynes@protonmail.com>
2022-01-29 21:21:58 +0800

    🥕 Added timestamps and unique quotes

--- model/musk.model.js
+++ model/musk.model.js
@@ -1,21 +1,25 @@
 module.exports = (mongoose) => {
   const muskModel = mongoose.model(
     "Musk",
-    mongoose.Schema({
-      _id: String,
-      quote: {
-        type: String,
-        required: true,
+    mongoose.Schema(
+      {
+        _id: String,
+        quote: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        quoteURL: {
+          type: String,
+          required: true,
+        },
+        addedBy: {
+          type: String,
+          required: true,
+        },
       },
-      quoteURL: {
-        type: String,
-        required: true,
-      },
-      addedBy: {
-        type: String,
-        required: true,
-      },
-    })
+      { timestamps: true }
+    )
   );
   return muskModel;
 };
